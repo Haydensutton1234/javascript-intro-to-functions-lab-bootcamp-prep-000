09c4d0e717bacea7453f0eafb53037ab85401d0a
Hayden Sutton <haydensutton1234@gmail.com>
2018-09-14 01:39:15 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -50,3 +50,16 @@ describe('sayHiToGrandma(string)', function() {
 })
 function shout(string) {
   return string.toUpperCase()
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
